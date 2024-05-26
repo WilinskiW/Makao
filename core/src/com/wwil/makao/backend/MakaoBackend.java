e70fbf3d72991155ecab164c9655a7987859500b
Wiktor Wilinski <wilinskiwiktor2005@gmail.com>
2024-05-26 10:40:43 +0200

    Added defence for human against K PIK

--- core/src/com/wwil/makao/backend/MakaoBackend.java
+++ core/src/com/wwil/makao/backend/MakaoBackend.java
@@ -78,6 +78,9 @@ public class MakaoBackend {
         }
 
         if (play.getAction() == Action.END) {
+            if(doesEventExist()){
+                event.setActive(false);
+            }
             return endTurn();
         }
 
